1b7e6ffa223d51786d8eb2a7c6d4d072a0bc413f
memoxmrdl <jmemox@gmail.com>
2013-07-26 22:18:28 -0500

    bootstrap project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tacomanager::Application.config.secret_key_base = '8b21b3fa179198f8b0469cd6d09c3f863027a701a5e77d88362574b726cb585aacb233a5c9b26cf1ec4b5967ccc7163aa40e39e8832b1dd9aa40690f600c6c28'
