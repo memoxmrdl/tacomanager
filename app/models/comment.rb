5e7e6b1c150690f49be154b84f3299ee0f09f223
memoxmrdl <jmemox@gmail.com>
2013-08-04 10:38:07 -0500

    Comment Model tests

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ActiveRecord::Base
   belongs_to :establishment
+
+  validates_presence_of :establishment_id
 end
--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,21 @@
 require "test_helper"
 
-class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+describe Comment do
+  let(:comment) { Comment.new }
+  let(:establishment) { establishments(:one) }
+
+  describe 'Validations' do
+    it 'must be valid' do
+      comment.comment = 'this is food good'
+      comment.establishment = establishment
+
+      comment.valid?.must_equal true
+    end
+
+    it 'must be invalid without attributes' do
+      comment.valid?.must_equal false
+
+      comment.errors[:establishment_id].wont_be_nil
+    end
+  end
 end
