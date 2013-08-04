281585b479a1a269238cc0786a09b5de73acaf2c
memoxmrdl <jmemox@gmail.com>
2013-08-04 01:12:37 -0500

    fix model Orderuser

--- app/models/orderuser.rb
+++ app/models/orderuser.rb
@@ -1,2 +1,4 @@
 class Orderuser < ActiveRecord::Base
+  belongs_to :order
+  belongs_to :user
 end
--- test/models/orderuser_test.rb
+++ test/models/orderuser_test.rb
@@ -1,7 +1,16 @@
 require "test_helper"
 
-class OrderuserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+describe Orderuser do
+  let(:orderuser) { Orderuser.new }
+  let(:order) { orders(:one) }
+  let(:user) { users(:user) }
+
+  describe 'Associatons' do
+    it 'must be it have a order and user' do
+       orderuser.user = user
+       orderuser.order = order
+
+       orderuser.valid?.must_equal true
+    end
+  end
 end
