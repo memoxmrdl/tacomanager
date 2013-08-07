bf42a0d6617d8419cc49fc9d89c414e13efa9354
memoxmrdl <guillermo.moreno@crowdint.com>
2013-08-07 01:40:51 -0500

    Tests DashboardController

--- test/controllers/dashboard/dashboard_controller_test.rb
+++ test/controllers/dashboard/dashboard_controller_test.rb
@@ -1,7 +1,21 @@
 require "test_helper"
 
-class Dashboard::DashboardControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+describe Dashboard::DashboardController do
+  describe 'GET index' do
+    it 'verify authenticate when a user is logged' do
+      login_user
+
+      get :index
+
+      must_response_with :success
+      must_render_template :index
+    end
+
+    it 'verify authenticate when a user is not logged' do
+      get :index
+
+      must_redirect_to root_path
+      flash[:alert].wont_be_nil
+    end
+  end
 end
