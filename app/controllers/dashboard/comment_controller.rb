500c32dab6c131d176006acce4328bc5974ed8fb
memoxmrdl <jmemox@gmail.com>
2013-08-07 18:06:13 -0500

    Functionality and tests CommentController

--- app/controllers/dashboard/comment_controller.rb
+++ app/controllers/dashboard/comment_controller.rb
@@ -1,2 +1,20 @@
-class Dashboard::CommentController < ApplicationController
+class Dashboard::CommentController < DashboardController
+  respond_to :js
+
+  def create
+    establishment = Establishment.find_by_id params[:establishment_id]
+
+    @comment = Comment.new comment_params
+    @comment.establishment = establishment
+
+    @comment = nil unless @comment.save
+
+    respond_with @comment
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:comment)
+  end
 end
